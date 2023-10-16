eb460d2fdd8228a61cbde3386a93b9ae158da34a
Arunachalam-08 <146165548+Arunachalam-08@users.noreply.github.com>
2023-10-16 11:34:41 +0530

    first commit

---
+++ greater,py.py
@@ -0,0 +1,16 @@
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+if a>b:
+      print("a is big")
+else:
+    print("b is big")
+
+
+######
+age=int(input("enter  your age:"))
+Gender=str(input("enter M for male,F for female:"))
+if(age>=18)&(Gender=="F" or Gender=="M"):
+           print("eligible for vote")
+else:
+    print("not eligible for vote")
+    
