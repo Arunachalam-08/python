6871f5864a399c10374e224524f09ab707fdb1e3
Arunachalam-08 <146165548+Arunachalam-08@users.noreply.github.com>
2023-10-17 10:52:11 +0530

    Add files via upload

---
+++ lartwo.py
@@ -0,0 +1,9 @@
+def largest(a,b,c):
+    if(a>b)&(a>c):
+        print("a and b is greater")
+    elif(b>c):
+         print("b and c is greater")
+    else:
+         print("a and c is greater")
+largest(12,13,18)
+    
